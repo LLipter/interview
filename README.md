e81ec32b2715fb7dcd810d49332bb15fa2a9bf5c
LLipter <katsudonn@163.com>
2020-05-24 01:25:44 +0800

    more update

--- README.md
+++ README.md
@@ -1,20 +1,26 @@
+# Latest Update
+
+ - 2020-05-23 17:34:29  [Transaction Isolation Levels](db/transcation-isolation.md) 
+ - 2020-05-23 17:00:22  [ACID Properties](db/acid.md) 
+ - 2020-05-23 13:29:25  [Friend Functions](cpp/friend_func.md) 
+
 # Table of Contents
 
 ### java
 
- - 2020-05-21  [Java Garbage Collection - Generation](java/gc-generation.md) 
- - 2020-05-21  [Java Garbage Collection - Aging](java/gc-aging.md) 
- - 2020-05-21  [Java Garbage Collection - Garbage Collectors](java/gc-collector.md) 
- - 2020-05-22  [Types of References in Java](java/gc-references.md) 
- - 2020-05-22  [GC roots](java/gc-roots.md) 
+ - 2020-05-21 20:05:38  [Java Garbage Collection - Generation](java/gc-generation.md) 
+ - 2020-05-21 20:05:38  [Java Garbage Collection - Aging](java/gc-aging.md) 
+ - 2020-05-21 21:55:42  [Java Garbage Collection - Garbage Collectors](java/gc-collector.md) 
+ - 2020-05-22 17:07:44  [Types of References in Java](java/gc-references.md) 
+ - 2020-05-22 17:29:28  [GC roots](java/gc-roots.md) 
 
 ### cpp
 
- - 2020-05-22  [Virtual Function](cpp/virtual-func.md) 
- - 2020-05-23  [Friend Functions](cpp/friend_func.md) 
+ - 2020-05-22 19:48:18  [Virtual Function](cpp/virtual-func.md) 
+ - 2020-05-23 13:29:25  [Friend Functions](cpp/friend_func.md) 
 
 ### db
 
- - 2020-05-23  [ACID Properties](db/acid.md) 
- - 2020-05-23  [Transaction Isolation Levels](db/transcation-isolation.md) 
+ - 2020-05-23 17:00:22  [ACID Properties](db/acid.md) 
+ - 2020-05-23 17:34:29  [Transaction Isolation Levels](db/transcation-isolation.md) 
 
--- index.py
+++ index.py
@@ -5,17 +5,18 @@ from operator import itemgetter
 
 if __name__ == '__main__':
     
-    included_dir = []
-    included_dir.append('java')
-    included_dir.append('cpp')
-    included_dir.append('db')
-
     data = {}
+    for dirname in os.listdir("."):
 
-    for dirname in included_dir:
-        filelist = os.listdir(dirname)
+        if not os.path.isdir(dirname):
+            continue
+
+        if dirname == '.git':
+            continue
+
         filedata = []
-        for filename in filelist:
+        first_create_time = datetime.datetime.max
+        for filename in os.listdir(dirname):
             link = os.path.join(dirname, filename)
             title = ''
             with open(link, 'r') as i_file:
@@ -24,16 +25,24 @@ if __name__ == '__main__':
                 pipe = pipe.stdout.readlines()
                 create_date = datetime.datetime.strptime(pipe[-3][12:-7].decode(), '%b %d %H:%M:%S %Y')
                 update_date = datetime.datetime.strptime(pipe[2][12:-7].decode(), '%b %d %H:%M:%S %Y')
+                first_create_time = min(first_create_time, create_date)
             filedata.append((title, link, create_date, update_date))
-        data[dirname] = filedata
+        data[dirname] = (filedata, first_create_time)
+    
+    latest_files = []
+    for i in [j[0] for j in data.values()]:
+        latest_files.extend(i)
+    latest_files.sort(key=itemgetter(2), reverse=True)
 
     with open("README.md", 'w') as o_file:
+        o_file.write('# Latest Update\n\n')
+        for i in range(3):
+            o_file.write(' - {2}  [{0}]({1}) \n'.format(latest_files[i][0], latest_files[i][1], latest_files[i][2]))
+        o_file.write('\n')
+
         o_file.write('# Table of Contents\n\n')
-        for dirname in included_dir:
+        for dirname, (filedata, first_create_time) in sorted(data.items(), key=lambda item: item[1][1]):
             o_file.write('### {0}\n\n'.format(dirname))
-            for title, link, create_date, update_date in sorted(data[dirname], key=itemgetter(2)):
-                o_file.write(' - {2}  [{0}]({1}) \n'.format(title, link, create_date.strftime("%Y-%m-%d")))
+            for title, link, create_date, update_date in sorted(filedata, key=itemgetter(2)):
+                o_file.write(' - {2}  [{0}]({1}) \n'.format(title, link, create_date))
             o_file.write('\n')
-
-
-
